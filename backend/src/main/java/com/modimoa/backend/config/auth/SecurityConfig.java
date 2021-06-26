45812b3085adc1665df5513be81a4904b2074082
Hyun-git <adius2845@naver.com>
2021-06-26 23:53:39 +0900

    Oatuth 공부2
    Oatuth 공부2

---
+++ backend/src/main/java/com/modimoa/backend/config/auth/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.modimoa.backend.config.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf()
+    }
+}
--- backend/src/main/java/com/modimoa/backend/repository/UserRepository.java
+++ backend/src/main/java/com/modimoa/backend/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.modimoa.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUser_email(String User_email);
 
 }
