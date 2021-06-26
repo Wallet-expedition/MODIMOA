caa1e1db172a6ca11f40ce35c87e8c76c3dea10d
LeeMir <manofgodmj@naver.com>
2021-06-26 16:52:01 +0900

    conditional route

--- frontend/src/Routes/Router.js
+++ frontend/src/Routes/Router.js
@@ -4,9 +4,24 @@ import { Route, Switch } from "react-router-dom";
 import { Intro, Main, Login } from "../Pages";
 
 const Router = () => {
+  const detectMobile = () => {
+    const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
+    ];
+
+    return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+    });
+  };
   return (
     <Switch>
-      <Route exact path="/" component={Intro} />
+      {detectMobile() && <Route exact path="/" component={Intro} />}
+      {!detectMobile() && <Route exact path="/" component={Main} />}
       <Route path="/main" component={Main} />
       <Route path="/auth" component={Login} />
     </Switch>
