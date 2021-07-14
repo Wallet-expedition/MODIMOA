5bca1e3b029d2845adec408dbd145f4d79cf3b80
LeeMir <manofgodmj@naver.com>
2021-07-14 20:06:20 +0900

    store 위치 변경

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-import Reducers from "./Store/Reducers";
-
-const store = createStore(Reducers);
+import store from "./Store/store";
 
 ReactDOM.render(
   <React.StrictMode>
