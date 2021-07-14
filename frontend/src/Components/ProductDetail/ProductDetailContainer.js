b1aee7ceb61c04abdf69ac94c8ca5e935527cc5d
LeeMir <manofgodmj@naver.com>
2021-07-14 20:07:36 +0900

    react-redux 연결

--- frontend/src/Components/ProductDetail/ProductDetailContainer.js
+++ frontend/src/Components/ProductDetail/ProductDetailContainer.js
@@ -1,24 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import ProductDetailPresenter from "./ProductDetailPresenter";
 
 const ProductDetailContainer = () => {
-  const item = {
-    "product_id": 1,
-    "mart_name": "SEVEN11",
-    "product_name": "르구르망)오레오밀크스낵30g",
-    "product_image": "https://www.7-eleven.co.kr/upload/product/0000090/426278.1.jpg",
-    "original_price": 2310,
-    "sale_price": 1000,
-    "sale_start_day": "2021-7-1",
-    "sale_end_day": "2021-7-31",
-    "sale_category": "OnePlusOne",
-    "gift_name": "",
-    "gift_image": "",
-    "gift_price": "",
-    "created_day": "2021-7-5",
-  };
-
+  const item = useSelector(state => state.productReducer.product);
+  /* TODO : store와 url끝의 martid가 일치하지 않을 경우, API 호출해서 갱신 */
   let sale_percent = Math.round((item.sale_price * 100) / item.original_price);
   return <ProductDetailPresenter item={item} sale_percent={sale_percent} />;
 };
