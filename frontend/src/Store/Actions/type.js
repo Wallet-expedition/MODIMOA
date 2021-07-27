e951491bb520630cc43a027f5b407602c62d8f09
Lee <43488305+poiu694@users.noreply.github.com>
2021-07-27 16:57:19 +0900

    Register Redux code

--- frontend/src/Store/Actions/type.js
+++ frontend/src/Store/Actions/type.js
@@ -3,4 +3,5 @@ export const LOGOUT_USER = "Logout_user";
 export const AUTH_USER = "Auth_user";
 export const WITHDRAW_USER = "Withdraw_user";
 export const GET_USER_INFO = "Get_user_info";
+export const REGITER_USER = "Register_user";
 export const SELECT_PRODUCT = "Select_product";
