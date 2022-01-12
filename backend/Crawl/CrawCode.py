b30c50303e43c5508d084704bed4f8811b004188
Hyun-git <76805070+Hyun-git@users.noreply.github.com>
2022-01-12 22:04:37 +0900

    Emart 와 CU 추가 완료
    크롤링 4개 편의점 추가 완료
    
    - 크롤링은 잘됨
    -데이터 정제만 조금 하면 될듯함

--- backend/Crawl/CrawCode.py
+++ backend/Crawl/CrawCode.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import json
+import re
 import pprint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException,ElementClickInterceptedException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import urllib.request
@@ -65,7 +66,7 @@ def seven11(cnt):
             dic[cnt] = {
                 "product_img" : product_img,
                 "product_name": product_name,
-                "product_prict":product_price,
+                "product_price":product_price,
                 "sale_price"  : sale_price,
                 "event" : event,
                 "mart_name" : mart_name
@@ -74,6 +75,7 @@ def seven11(cnt):
             with open("data.json", 'w',encoding='UTF-8') as outfile:
                 json.dump(dic, outfile, ensure_ascii=False,indent='\t')
             time.sleep(1)
+    print("End Seven")
     return cnt
 
 def gs25(cnt):
@@ -103,7 +105,7 @@ def gs25(cnt):
                 except NoSuchElementException:
                     pass
                 num += 1
-                if num is 8:
+                if num == 8:
                     num = 1
                     driver.execute_script('goodsPageController.moveControl(1);')
                 cnt += 1
@@ -118,7 +120,7 @@ def gs25(cnt):
                 dic[cnt] = {
                     "product_img" : product_img,
                     "product_name": product_name,
-                    "product_prict":product_price,
+                    "product_price":product_price,
                     "sale_price"  : sale_price,
                     "event" : event,
                     "mart_name" : mart_name
@@ -130,52 +132,142 @@ def gs25(cnt):
                 break
             except StaleElementReferenceException:
                 time.sleep(2)
+    print("End Gs")
     return cnt
 
 def emart24(cnt):
     driver.get('https://www.emart24.co.kr/product/eventProduct.asp')
-
+    mart_name = "emart24"
+    pagenum = driver.find_element_by_css_selector("#regForm > div.section > div.eventProduct > div.paging > a:nth-child(14)").get_attribute('href')
+    pagenum = re.findall(r'\d+', pagenum)
+    pagenum = int(pagenum[0])
     num = 1
+    page = 1
     while 1:
         try:
-            cnt+=1
+            
             # 상품의 이름 및 가격 (line1: name, line2: price, line3: img)
-            print(driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.tabContArea > ul > li:nth-child(%s) > div > p.productDiv' % num).text)
-            print(driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.tabContArea > ul > li:nth-child(%s) > div > p.price' % num).text)
+                                                                
+            product_name = (driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.tabContArea > ul > li:nth-child(%s) > div > p.productDiv' % num).text)
+            product_price = driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.tabContArea > ul > li:nth-child(%s) > div > p.price' % num).text
+            product_price = product_price.replace(",","")
+            product_price = product_price.replace("원","")
+
             try:
-                print(driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.tabContArea > ul > li:nth-child(%s) > div > p.productImg' % num).find_element_by_tag_name('img').get_attribute('src'))
+                product_img = (driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.tabContArea > ul > li:nth-child(%s) > div > p.productImg' % num).find_element_by_tag_name('img').get_attribute('src'))
             except NoSuchElementException:
                 pass
-            eventtype = driver.find_element_by_xpath('//*[@id="regForm"]/div[2]/div[3]/div[2]/ul/li[8]/div/div/p/img').get_attribute('alt')
-            print(eventtype)
+                                                     
+            eventtype = driver.find_element_by_xpath('//*[@id="regForm"]/div[2]/div[3]/div[2]/ul/li[%s]/div/div/p/img'%num).get_attribute('alt')
             if '2 + 1 뱃지'in eventtype:
                 eventtype = '2+1'
+                product_price = int(product_price)
+                sale_price = int(product_price)*2//3
             elif 'SALE 뱃지'in eventtype:
+                product_price = product_price.replace(" →"," ")
+                product_price = product_price.split()
+                sale_price = int(product_price[1])
+                product_price = int(product_price[0])
                 eventtype = 'sale'
-            elif 'X2 더블 뱃지'in eventtype:
-                eventtype = 'dum'
             elif '1 + 1 뱃지 이미지'in eventtype:
                 eventtype = '1+1'
+                product_price = int(product_price)
+                sale_price = int(product_price)//2
             elif '3 + 1 뱃지'in eventtype:
                 eventtype = '3+1'
+                product_price = int(product_price)
+                sale_price = int(product_price)*3//4
             else:
                 eventtype = 'error!'
-            print(eventtype)
+
             num += 1
-            if num is 16:
-                num = 1
-                driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.paging > a.next.bgNone > alt').click()
+            if(eventtype != 'error!'):
+                cnt+=1
+                dic[cnt] = {
+                        "product_img" : product_img,
+                        "product_name": product_name,
+                        "product_price":product_price,
+                        "sale_price"  : sale_price,
+                        "event" : eventtype,
+                        "mart_name" : mart_name
+                    }
+                with open("data.json", 'w',encoding='UTF-8') as outfile:
+                        json.dump(dic, outfile, ensure_ascii=False,indent='\t')
+
+            if num == 16:
+                num = 1             
+                driver.find_element_by_css_selector('#regForm > div.section > div.eventProduct > div.paging > a.next.bgNone ').click()
+                page+=1
+            if(pagenum == page):
+                break
         except NoSuchElementException:
             break
         except StaleElementReferenceException:
             time.sleep(2)
-
+    print("End Emart")
     return cnt
 
+def CU(cnt):
+    mart_name = "CU"
+    num = 0
+
+    driver.get("https://cu.bgfretail.com/event/plus.do")
+    time.sleep(3)
+    while(1):
+        try:
+            driver.find_element_by_css_selector("#contents > div.relCon > div.prodListWrap > div > div.prodListBtn-w > a").click()
+        except ElementClickInterceptedException:
+            time.sleep(2)
+            continue
+        except  StaleElementReferenceException:
+            time.sleep(2)
+            continue
+        except NoSuchElementException:
+            break
+    col = 17
+    while(1):
+        try:
+            num+= 1
+            cnt +=1
+           
+                                                                 #contents > div.relCon > div.prodListWrap > ul:nth-child(272) > li:nth-child(39) > p.prodName
+                                                                 #contents > div.relCon > div.prodListWrap > ul:nth-child(289) > li:nth-child(1) > p.prodName 
+            product_name = (driver.find_element_by_css_selector('#contents > div.relCon > div.prodListWrap > ul:nth-child(%s) > li:nth-child(%s) > p.prodName' %(col , num)).text)
+            product_price = driver.find_element_by_css_selector('#contents > div.relCon > div.prodListWrap > ul:nth-child(%s) > li:nth-child(%s) > p.prodPrice' %(col , num)).text
+            product_price = product_price.replace(",","")
+            product_price = int(product_price.replace("원",""))
+            product_img = driver.find_element_by_css_selector('#contents > div.relCon > div.prodListWrap > ul:nth-child(%s) > li:nth-child(%s) > div > a > img' %(col , num)).get_attribute("src")
+            event = driver.find_element_by_css_selector('#contents > div.relCon > div.prodListWrap > ul:nth-child(%s) > li:nth-child(%s) > ul > li ' %(col , num)).text
+            if (event == "1+1"):
+                sale_price = product_price//2
+            elif (event == "2+1"):
+                sale_price = product_price*2//3
+            else:
+                sale_price = 0
+            print(product_name,product_img,product_price,event)
+            dic[cnt] = {
+                        "product_img" : product_img,
+                        "product_name": product_name,
+                        "product_price":product_price,
+                        "sale_price"  : sale_price,
+                        "event" : event,
+                        "mart_name" : mart_name
+                    }
+            with open("data.json", 'w',encoding='UTF-8') as outfile:
+                    json.dump(dic, outfile, ensure_ascii=False,indent='\t')
+            if(num == 40):
+                col +=17
+                num = 1
+        except NoSuchElementException:
+            break
         
+    print("End CU")
+    return cnt
+
 
         
 cnt = 0
-cnt = emart24(cnt)
+cnt = CU(cnt)
 cnt = gs25(cnt)
+cnt = emart24(cnt)
 cnt = seven11(cnt)
