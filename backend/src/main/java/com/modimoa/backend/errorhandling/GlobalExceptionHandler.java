fc2a66401881308bf39fae846a660a8c2d314235
suin <ei654028@gmail.com>
2022-01-14 16:32:10 +0900

    custom error

--- backend/src/main/java/com/modimoa/backend/errorhandling/GlobalExceptionHandler.java
+++ backend/src/main/java/com/modimoa/backend/errorhandling/GlobalExceptionHandler.java
@@ -1,39 +1,18 @@
 package com.modimoa.backend.errorhandling;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(InvalidQueryException.class)
-    public ResponseEntity<ErrorResponse> HandleInvalidQueryException(InvalidQueryException ex){
-        log.error("HandleInvalidQueryException",ex);
-
-        ErrorResponse response = new ErrorResponse(ex.getErrorCode());
-
-        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
-    }
-
-    @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<ErrorResponse> ObjectNotFoundQueryException(ObjectNotFoundException ex){
-        log.error("ObjectNotFoundException",ex);
-
-        ErrorResponse response = new ErrorResponse(ex.getErrorCode());
-
-        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
-    }
-
-    @ExceptionHandler(MemberConflictException.class)
-    public ResponseEntity<ErrorResponse> MemberConflictException(MemberConflictException ex){
-        log.error("MemberConflictException",ex);
-
-        ErrorResponse response = new ErrorResponse(ex.getErrorCode());
-
-        return new ResponseEntity<>(response, HttpStatus.valueOf(ex.getErrorCode().getStatus()));
+    @ExceptionHandler(value = { CustomException.class })
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 }
