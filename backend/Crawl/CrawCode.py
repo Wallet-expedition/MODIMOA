72e5252875092a3a1c49a290fde19b42020195b6
Hyun-git <76805070+Hyun-git@users.noreply.github.com>
2022-01-14 18:03:36 +0900

    Crwaling 데이터 정제 및 CSV로 만들기 완료
    -모든 편의점 크롤링완료
    -json을 csv 형식으로 바꾸기도 완료
    
    CSV내에서 해야할 것
    - 수동입력 해야함 
            세븐: 증정, 할인
    	Emart: 덤증정 상품(이미지 없음)
    	Gs : 덤증정행사 (마우스  올려야 이미지 보임)
    - 왼쪽에 한열 삽입해서 primaryKey(product_id) 키 만들어주기 1~ 총개수까지

--- backend/Crawl/CrawCode.py
+++ backend/Crawl/CrawCode.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import json
 import re
-import pprint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException,ElementClickInterceptedException
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import urllib.request
 from selenium.webdriver.support.ui import WebDriverWait
+import csv
+import datetime as dt
+import calendar
+
+
 driver = webdriver.Chrome('/Users/ihyeongwang/Desktop/crawling/chromedriver11')
 
 dic = {
@@ -17,8 +20,8 @@ dic = {
 }
 
 
-def seven11(cnt):
-    mart_name = "seven11"
+def SEVEN11(cnt):
+    mart_name = "SEVEN11"
 
     driver.get("https://www.7-eleven.co.kr/product/presentList.asp")
     time.sleep(3)
@@ -32,7 +35,7 @@ def seven11(cnt):
         while(1):
             try: 
                 driver.find_element_by_class_name('btn_more').click()
-                time.sleep(1)
+                time.sleep(2)
             except:
                 break
 
@@ -44,42 +47,40 @@ def seven11(cnt):
             cnt += 1
 
             if(j<15):
-                product_img = driver.find_element_by_xpath("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/ul/li[{}]/div/img".format(str(j)))
+                product_img = driver.find_element_by_css_selector("#listUl > li:nth-child({}) > div > img".format(str(j)))
                 product_img = product_img.get_attribute("src")
-                product_name = driver.find_element_by_xpath("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/ul/li[{}]/div/div/div[1]".format(str(j))).text
-                product_price = driver.find_element_by_xpath("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/ul/li[{}]/div/div/div[2]/span".format(str(j))).text
+                product_name = driver.find_element_by_css_selector("#listUl > li:nth-child({}) > div > div > div.name".format(str(j))).text
+                product_price = driver.find_element_by_css_selector("#listUl > li:nth-child({}) > div > div > div.price".format(str(j))).text
                 product_price = int(product_price.replace(",",""))
             else:
-                product_img = driver.find_element_by_xpath("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/ul/li[{}]/div/div/img".format(str(j)))
+                product_img = driver.find_element_by_css_selector("#listUl > li:nth-child({}) > div >div> img".format(str(j)))
                 product_img = product_img.get_attribute("src")
-                product_name = driver.find_element_by_xpath("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/ul/li[{}]/div/div/div/div[1]".format(str(j))).text
-                product_price = driver.find_element_by_xpath("/html/body/div[2]/div[2]/form/div[3]/div[2]/div/ul/li[{}]/div/div/div/div[2]/span".format(str(j))).text
+                product_name = driver.find_element_by_css_selector("#listUl > li:nth-child({}) > div >div> div > div.name".format(str(j))).text
+                product_price = driver.find_element_by_css_selector("#listUl > li:nth-child({}) > div > div>div > div.price".format(str(j))).text
                 product_price = int(product_price.replace(",",""))
 
             if(i == 1):
-                event = "1+1"
+                event = "OnePlusOne"
                 sale_price = product_price//2
             elif(i==2):
-                event = "2+1"
+                event = "TwoPlusOne"
                 sale_price = product_price*2//3
 
             dic[cnt] = {
-                "product_img" : product_img,
+                "product_image" : product_img,
                 "product_name": product_name,
-                "product_price":product_price,
+                "original_price":product_price,
                 "sale_price"  : sale_price,
-                "event" : event,
+                "sale_category" : event,
                 "mart_name" : mart_name
             }
-            print(dic[cnt])
             with open("data.json", 'w',encoding='UTF-8') as outfile:
                 json.dump(dic, outfile, ensure_ascii=False,indent='\t')
-            time.sleep(1)
     print("End Seven")
     return cnt
 
-def gs25(cnt):
-    mart_name = "gs25"
+def GS25(cnt):
+    mart_name = "GS25"
     driver.get('http://gs25.gsretail.com/gscvs/ko/products/event-goods')
 
     for i in range(1,3):
@@ -88,13 +89,13 @@ def gs25(cnt):
         time.sleep(2)
         while 1:
             try:
-                
-                product_name= (driver.find_element_by_css_selector('#contents > div.cnt > div.cnt_section.mt50 > div > div > div:nth-child(3) > ul > li:nth-child(%s) > div > p.tit' % num).text)
                 if(i == 1):
+                    product_name= (driver.find_element_by_css_selector('#contents > div.cnt > div.cnt_section.mt50 > div > div > div:nth-child(3) > ul > li:nth-child(%s) > div > p.tit' % num).text)
                     product_price= driver.find_element_by_css_selector('#contents > div.cnt > div.cnt_section.mt50 > div > div > div:nth-child(3) > ul > li:nth-child(%s) > div > p.price' % num).text
                     product_price = product_price.replace(",","")
                     product_price = int(product_price.replace("원",""))
                 else:
+                    product_name= (driver.find_element_by_css_selector('#contents > div.cnt > div.cnt_section.mt50 > div > div > div:nth-child(5) > ul > li:nth-child(%s) > div > p.tit' % num).text)
                     product_price= driver.find_element_by_css_selector('#contents > div.cnt > div.cnt_section.mt50 > div > div > div:nth-child(5) > ul > li:nth-child(%s) > div > p.price' % num).text
                     product_price = product_price.replace(",","")
                     product_price = int(product_price.replace("원",""))
@@ -111,18 +112,18 @@ def gs25(cnt):
                 cnt += 1
                 
                 if(i == 1):
-                    event = "1+1"
+                    event = "OnePlusOne"
                     sale_price = product_price//2
                 elif(i==2):
-                    event = "2+1"
+                    event = "TwoPlusOne"
                     sale_price = product_price*2//3
 
                 dic[cnt] = {
-                    "product_img" : product_img,
+                    "product_image" : product_img,
                     "product_name": product_name,
-                    "product_price":product_price,
+                    "original_price":product_price,
                     "sale_price"  : sale_price,
-                    "event" : event,
+                    "sale_category" : event,
                     "mart_name" : mart_name
                 }
                 print(dic)
@@ -135,9 +136,9 @@ def gs25(cnt):
     print("End Gs")
     return cnt
 
-def emart24(cnt):
+def EMART24(cnt):
     driver.get('https://www.emart24.co.kr/product/eventProduct.asp')
-    mart_name = "emart24"
+    mart_name = "EMART24"
     pagenum = driver.find_element_by_css_selector("#regForm > div.section > div.eventProduct > div.paging > a:nth-child(14)").get_attribute('href')
     pagenum = re.findall(r'\d+', pagenum)
     pagenum = int(pagenum[0])
@@ -160,7 +161,7 @@ def emart24(cnt):
                                                      
             eventtype = driver.find_element_by_xpath('//*[@id="regForm"]/div[2]/div[3]/div[2]/ul/li[%s]/div/div/p/img'%num).get_attribute('alt')
             if '2 + 1 뱃지'in eventtype:
-                eventtype = '2+1'
+                eventtype = 'TwoPlusOne'
                 product_price = int(product_price)
                 sale_price = int(product_price)*2//3
             elif 'SALE 뱃지'in eventtype:
@@ -168,13 +169,13 @@ def emart24(cnt):
                 product_price = product_price.split()
                 sale_price = int(product_price[1])
                 product_price = int(product_price[0])
-                eventtype = 'sale'
+                eventtype = 'Discount'
             elif '1 + 1 뱃지 이미지'in eventtype:
-                eventtype = '1+1'
+                eventtype = 'OnePlusOne'
                 product_price = int(product_price)
                 sale_price = int(product_price)//2
             elif '3 + 1 뱃지'in eventtype:
-                eventtype = '3+1'
+                eventtype = 'ThreePlusOne'
                 product_price = int(product_price)
                 sale_price = int(product_price)*3//4
             else:
@@ -184,11 +185,11 @@ def emart24(cnt):
             if(eventtype != 'error!'):
                 cnt+=1
                 dic[cnt] = {
-                        "product_img" : product_img,
+                        "product_image" : product_img,
                         "product_name": product_name,
-                        "product_price":product_price,
+                        "original_price":product_price,
                         "sale_price"  : sale_price,
-                        "event" : eventtype,
+                        "sale_category" : eventtype,
                         "mart_name" : mart_name
                     }
                 with open("data.json", 'w',encoding='UTF-8') as outfile:
@@ -239,18 +240,20 @@ def CU(cnt):
             product_img = driver.find_element_by_css_selector('#contents > div.relCon > div.prodListWrap > ul:nth-child(%s) > li:nth-child(%s) > div > a > img' %(col , num)).get_attribute("src")
             event = driver.find_element_by_css_selector('#contents > div.relCon > div.prodListWrap > ul:nth-child(%s) > li:nth-child(%s) > ul > li ' %(col , num)).text
             if (event == "1+1"):
+                event = "OnePlusOne"
                 sale_price = product_price//2
             elif (event == "2+1"):
+                event = "TwoPlusOne"
                 sale_price = product_price*2//3
             else:
                 sale_price = 0
             print(product_name,product_img,product_price,event)
             dic[cnt] = {
-                        "product_img" : product_img,
+                        "product_image" : product_img,
                         "product_name": product_name,
-                        "product_price":product_price,
+                        "original_price":product_price,
                         "sale_price"  : sale_price,
-                        "event" : event,
+                        "sale_category" : event,
                         "mart_name" : mart_name
                     }
             with open("data.json", 'w',encoding='UTF-8') as outfile:
@@ -263,11 +266,39 @@ def CU(cnt):
         
     print("End CU")
     return cnt
-
-
         
 cnt = 0
 cnt = CU(cnt)
-cnt = gs25(cnt)
-cnt = emart24(cnt)
-cnt = seven11(cnt)
+cnt = GS25(cnt)
+cnt = EMART24(cnt)
+cnt = SEVEN11(cnt)
+driver.close()
+
+with open('data.json', 'r', encoding = 'utf-8') as input_file, open('data.csv', 'w', newline = '') as output_file :
+    data = json.load(input_file)
+    
+    	# "product_img": "https://www.7-eleven.co.kr/upload/product/8801069/415048.1.jpg",
+		# "product_name": "PB)마쉼흑당라떼320ml",
+		# "product_price": 2300,
+		# "sale_price": 1533,
+		# "event": "2+1",
+		# "mart_name": "seven11" 
+
+    f = csv.writer(output_file)
+    # csv 파일에 header 추가
+    f.writerow(["mart_name"	,"product_name"	,"product_image","original_price","sale_price",	"sale_start_day","sale_end_day"	,"sale_category","gift_name","gift_image",	"gift_price",	"created_day"])
+    # write each row of a json file
+    now = dt.datetime.now()
+    lastday = calendar.monthrange(now.year, now.month)[1]
+    created_day = str(now.year)+"-"+str(now.month)+"-"+str(now.day)
+    sale_start_day = str(now.year)+"-"+str(now.month)+"-1"
+    sale_end_day = str(now.year)+"-"+str(now.month)+"-"+str(lastday)
+
+
+
+ 	
+    for k in range(1,len(data)+1):
+        try:
+            f.writerow([data[str(k)]["mart_name"], data[str(k)]["product_name"], data[str(k)]["product_image"], data[str(k)]["original_price"], data[str(k)]["sale_price"], sale_start_day, sale_end_day, data[str(k)]["sale_category"], " " , " ", " " ,created_day])
+        except KeyError:
+            continue
