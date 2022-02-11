d97d8697244826875972374bab05fd52d2e580f9
LeeMir <manofgodmj@gmail.com>
2022-02-12 02:14:25 +0900

    [Feat] Loading 호출 in Product List

--- frontend/src/Components/ProductList/ProductListPresenter.js
+++ frontend/src/Components/ProductList/ProductListPresenter.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Product from "../Product";
+import Loading from "../Loading";
 
-const ProductListPresenter = ({ list, listComponent }) => {
+const ProductListPresenter = ({ list, listComponent, isLoading }) => {
   return (
-    <main className="product-list-container" ref={listComponent}>
-      {list.map((item) => {
-        return (
-          <Product
-            key={item.productId}
-            id={item.productId}
-            mart_name={item.martName}
-            product_name={item.productName}
-            product_image={item.productImage}
-            original_price={item.originalPrice}
-            sale_price={item.salePrice}
-          />
-        );
-      })}
+    <main className="product-list-container-container">
+      <div className="product-list-container" ref={listComponent}>
+        {list.map((item) => {
+          return (
+            <Product
+              key={item.productId}
+              id={item.productId}
+              mart_name={item.martName}
+              product_name={item.productName}
+              product_image={item.productImage}
+              original_price={item.originalPrice}
+              sale_price={item.salePrice}
+            />
+          );
+        })}
+      </div>
+      {isLoading && <Loading />}
     </main>
   );
 };
